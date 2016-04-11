5d60584ef8a6564944c6358ddfd960178dd1ba0a
Lawrence Norton <lawrence@lawrencenorton.com>
2016-04-11 13:48:51 -0400

    updated minor things

--- gulp/pug.js
+++ gulp/pug.js
@@ -7,6 +7,11 @@ var pug = require('gulp-pug');
 gulp.task('pug', function() {
     return gulp.src(['src/views/*.pug', '!./src/views/_*'])
         .pipe(plumber())
-        .pipe(pug({ pretty: true}))
+        .pipe(pug({
+            pretty: true,
+            data: {
+                name : 'stephen'
+            }
+        }))
         .pipe(gulp.dest('dist'));
 });
