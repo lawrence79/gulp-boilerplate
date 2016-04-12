d74b84b09d77da364d7da3939fa5178ce9affc7f
Lawrence Norton <lawrence@lawrencenorton.com>
2016-04-12 11:30:07 -0400

    page.js vs grapnel

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 var _ = require('underscore');
 var $ = require('jquery')
-
+var page = require('page');
 var vivus = require('vivus');
-var Grapnel = require('grapnel');
-var catchLinks = require('catch-links');
 
 var app = app || {};
 
@@ -11,45 +9,33 @@ app = (function() {
     'use strict';
 
     var _cache = {},
-    router = new Grapnel({ pushState: true }),
+
     _init = function() {
-        _initRouter();
-        console.log('app loaded');
         new vivus('box', { duration: 300, type: 'oneByOne' });
+        _initRouter();
         _addEvents();
     },
+
     _addEvents = function() {
         $('.example-link').click(function(e) {
-            router.navigate('/examples');
             e.preventDefault();
         });
     },
+
     _initRouter = function() {
-
-        router.get('/', _index);
-        router.get('/examples', _example);
-        // wildcard route - matches all
-        router.get('/*', function(req, e) {
-          $(window).scrollTop(0);
-
-          // if the wildcard route is the only one that matches, it's a 404
-          // just route to the homepage for now
-          if (!e.parent()) {
-            router.navigate('/');
-          }
-        });
-
-        // Handle all internal link clicks with Grapnel and ignore external URLs
-        catchLinks(window, function(href) {
-            router.navigate(href);
-        });
+        page('/', _index);
+        page('*', _notfound);
+        page();
     },
+
     _notfound = function() {
         console.log('not found');
     },
+
     _example = function() {
         console.log('on example page');
     },
+
     _index = function() {
         console.log('router loaded');
     };
