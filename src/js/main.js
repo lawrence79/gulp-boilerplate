c2cf0399c92f315c46980b181cb89249978841be
Lawrence Norton <lawrence@lawrencenorton.com>
2016-04-13 11:13:28 -0400

    removed client side routing

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 var _ = require('underscore');
 var $ = require('jquery')
-// optional
-var page = require('page');
-var vivus = require('vivus');
+
 
 var app = app || {};
 
@@ -10,21 +8,12 @@ app = (function() {
     'use strict';
 
     var _cache = {
-        box: $('#box')
     },
 
     _init = function() {
-        if(_cache.box.length > 0) {
-            new vivus('box', { duration: 300, type: 'oneByOne' });
-        }
-        _addEvents();
     },
 
     _addEvents = function() {
-
-    },
-
-    _initRouter = function() {
     };
 
     return {
